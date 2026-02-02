e813ac9c10716033cee7756da98b8bc6583deb51
Mittal <shmittal@ciena.com>
2026-02-02 21:29:52 +0530

    Initial Commit

---
+++ src/main/java/org/example/Alien.java
@@ -0,0 +1,15 @@
+ package org.example;
+public class Alien {
+
+   // private Laptop laptop;
+   public Alien(){
+       System.out.println("a contructor");
+   }
+
+    public void build(){
+        System.out.println("Working");
+        //laptop.compile();
+    }
+
+
+}
---
+++ src/main/java/org/example/Laptop.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Laptop {
+
+    public Laptop(){
+        System.out.println("laptop contructor");
+    }
+
+    public void complie(){
+        System.out.println("compiling");
+    }
+
+}
